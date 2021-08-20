d1b1b5094ed740e32579162320bd53ee1b004b14
Jesper Lundqvist <jesper.lundqvist@eyevinn.se>
2021-08-20 12:41:26 +0200

    Updated sample

--- sample/main.swift
+++ sample/main.swift
@@ -7,13 +7,24 @@
 
 import Foundation
 import swift_srt
+import VideoToolbox
 
 let url = URL(string: "srt://0.0.0.0:1234")!
 let socket = SRTSocket()
+
+// SRTO_RCVSYN to true
+socket.set(option: .rcvsyn, value: 1)
+
 try! socket.bind(to: url)
 socket.listen(withBacklog: 2)
+
 print("Listening to " + url.absoluteString)
 
 let fd = socket.accept()
-let chunk = fd.read(dataWithSize: 1316)
-print(chunk.base64EncodedString())
+
+for _ in 1...100 {
+    let chunk = fd.read(dataWithSize: 1316)
+    print("Recieved message of length \(chunk.count)")
+}
+
+socket.close()
