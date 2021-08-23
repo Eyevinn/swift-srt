90c71e3e4f3d62fa02dc3876d88d9f5bcf02285f
Jesper Lundqvist <jesper.lundqvist@eyevinn.se>
2021-08-23 09:52:07 +0200

    Added descriptions for each error

--- swift-srt/Error.swift
+++ swift-srt/Error.swift
@@ -49,3 +49,90 @@ public enum SRTError: Int, Error {
     case eboundsock = 5001
     case eresource = 3000
 }
+
+extension SRTError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "Unknown error."
+        case .esockfail:
+            return "An error occurred when trying to call a system function on an internally used UDP socket."
+        case .etimeout:
+            return "The operation timed out."
+        case .econnsetup:
+            return "General setup error resulting from internal system state."
+        case .enoconn:
+            return "The socket is not connected."
+        case .econnsock:
+            return "The socket is currently connected and therefore performing the required operation is not possible."
+        case .erdperm:
+            return "Read permission was denied when trying to read from file."
+        case .easyncfail:
+            return "General asynchronous failure."
+        case .esclosed:
+            return "A socket that was vital for an operation called in blocking mode has been closed during the operation."
+        case .epollempty:
+            return "The epoll container currently has no subscribed sockets."
+        case .econnrej:
+            return "Connection has been rejected."
+        case .efile:
+            return "General filesystem error."
+        case .einvrdoff:
+            return "Failure when trying to read from a given position in the file."
+        case .einvop:
+            return "Invalid operation performed for the current state of a socket."
+        case .erdvnoserv:
+            return "The required operation cannot be performed when the socket is set to rendezvous mode."
+        case .einvsock:
+            return "The API function required an ID of an entity (socket or group) and it was invalid."
+        case .enolisten:
+            return "The socket passed for the operation is required to be in the listen state."
+        case .eduplisten:
+            return "The port tried to be bound for listening is already busy."
+        case .epeererr:
+            return "Receiver peer is writing to a file that the agent is sending."
+        case .ewrperm:
+            return "Write permission was denied when trying to write to a file."
+        case .einvalbufferapi:
+            return "The function was used incorrectly in the stream (buffer) API."
+        case .erdvunbound:
+            return "An attempt was made to connect to a socket set to rendezvous mode that was not first bound."
+        case .esysobj:
+            return "System was unable to allocate system specific objects."
+        case .enoserver:
+            return "Connection timed out while attempting to connect to the remote address."
+        case .easyncsnd:
+            return "Sending operation is not ready to perform."
+        case .einvwroff:
+            return "Failed to set position in the written file."
+        case .einvalmsgapi:
+            return "The function was used incorrectly in the message API."
+        case .eunboundsock:
+            return "The operation to be performed on a socket requires that it first be explicitly bound."
+        case .econgest:
+            return "With SRTO_TSBPDMODE and SRTO_TLPKTDROP set to true, some packets were dropped by sender."
+        case .enobuf:
+            return "System was unable to allocate memory for buffers."
+        case .econnlost:
+            return "The socket was properly connected, but the connection has been broken."
+        case .einvparam:
+            return "Call parameters for API functions have some requirements that were not satisfied."
+        case .einvpollid:
+            return "The epoll ID passed to an epoll function is invalid."
+        case .easyncrcv:
+            return "Receiving operation is not ready to perform."
+        case .esecfail:
+            return "A possible tampering with the handshake packets was detected, or encryption request wasn't properly fulfilled."
+        case .elargemsg:
+            return "Size exceeded."
+        case .econnfail:
+            return "General connection failure of unknown details."
+        case .ethread:
+            return "System was unable to spawn a new thread when requried."
+        case .eboundsock:
+            return "The socket is currently bound and the required operation cannot be performed in this state."
+        case .eresource:
+            return "System or standard library error reported unexpectedly for unknown purpose."
+        }
+    }
+}
